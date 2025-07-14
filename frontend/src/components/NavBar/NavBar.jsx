2730a598ea5c941bd549c9f71e22b413f3ca9ab1
Aravind Kumar <arvind.arvindkumar1994@gmail.com>
2025-07-14 21:07:44 +0530

    90% of  frontend  is done. Few small tasks are pending

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Logo from "../Logo/Logo";
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
+  const navigate = useNavigate();
+
   return (
     <div className="fixed top-0 left-0 right-0 bg-white shadow-lg flex items-center justify-between px-1">
       <div className="left flex items-center">
@@ -10,7 +13,26 @@ function NavBar() {
           job<span className="font-semibold">Verse</span>
         </p>
       </div>
-      <div className="right">buttons</div>
+      <div className="right">
+        <div className="flex gap-2">
+          <button
+            className="large:px-5 large:py-2.5 px-3 py-1 text-sm tablet:px-4 tablet:py-2 tablet:py cursor-pointer text-center font-semibold border  border-blue-300 text-blue-500 rounded-sm"
+            onClick={() => {
+              navigate("/auth", { state: { type: "login" } });
+            }}
+          >
+            Login
+          </button>
+          <button
+            className="large:px-5 large:py-2.5 px-3 py-1 text-sm tablet:px-4  tablet:py-2 cursor-pointer text-center font-semibold bg-blue-500 rounded-sm text-white"
+            onClick={() => {
+              navigate("/auth", { state: { type: "signup" } });
+            }}
+          >
+            Sign up
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function Auth() {
+  const {
+    state: { type },
+  } = useLocation();
+
+  console.log(type);
+
   return <div>Auth</div>;
 }
 
